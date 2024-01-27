3b71d8b92eec47baf8fd871a8e167c3b9cadde5c
Greg Field <gregfield@g.ucla.edu>
2024-01-27 11:51:52 -0800

    updating SimulateFieldStage

--- base-package/+fieldlab/+rigs/SimulatedFieldStage.m
+++ base-package/+fieldlab/+rigs/SimulatedFieldStage.m
@@ -9,23 +9,15 @@ classdef SimulatedFieldStage < symphonyui.core.descriptions.RigDescription
             
             daq = HekaSimulationDaqController();
             obj.daqController = daq;
-            
-            % Rig name and laboratory.
-            rigDev = manookinlab.devices.RigPropertyDevice('ManookinLab','SimulatedStage');
-            obj.addDevice(rigDev);
-            
+                        
             amp1 = MultiClampDevice('Amp1', 1).bindStream(daq.getStream('ao0')).bindStream(daq.getStream('ai0'));
             obj.addDevice(amp1);
-
-            % Add an analog trigger device to simulate the MEA.
-            trigger = UnitConvertingDevice('ExternalTrigger', 'V').bindStream(daq.getStream('ao1'));
-            obj.addDevice(trigger);
                         
             frameMonitor = UnitConvertingDevice('Frame Monitor', 'V').bindStream(obj.daqController.getStream('ai7'));
             obj.addDevice(frameMonitor);
             
             geneticVideoDisplay = manookinlab.devices.VideoDevice('micronsPerPixel', 2.4);                 
-            obj.addDevice(microdisplay);
+            obj.addDevice(geneticVideoDisplay);
             
         end
     end
